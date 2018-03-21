b0bfc166321fc59040ae43ac47a9d41f16551728
yubin <yubin@tuhu>
2018-03-21 15:40:15 +0800

    webpack4-demo

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import App from './App';
+
+// function component() {
+//   var element = document.createElement('div');
+//   element.innerHTML = _.join(['Hello', 'webpackkkkkkkkkkkkkkkkk'], ' ');
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
+
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "./index.html"
+    })
+  ]
+};
